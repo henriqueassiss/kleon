888c756cd99f779c5cf81cf172ae4394c0815e7b
henriqueassiss <contato.henriqueassiss@gmail.com>
2023-04-04 15:00:32 -0300

    feat: add pages

--- frontend/src/components/trending/index.tsx
+++ frontend/src/components/trending/index.tsx
@@ -6,10 +6,12 @@ import styles from './styles.module.scss';
 import Image from 'next/image';
 import loader from '@public/svgs/LoaderBlue.svg';
 import Wrapper from '@/components/layouts/wrapper';
+import { useState } from 'react';
 
 const Trending = () => {
+	const [offset, setOffset] = useState(0);
 	const { data, isLoading } = useSWR<TResponse<TCardDefault[]>>(
-		'/contact?offset=12',
+		'/contact?offset=' + offset,
 		getReq,
 	);
 
@@ -40,6 +42,19 @@ const Trending = () => {
 							className={styles.loading}
 						/>
 					)}
+				</div>
+
+				<div className={styles.pages}>
+					<button
+						onClick={() => setOffset(0)}
+						className={offset === 0 ? styles.active : ''}>
+						1
+					</button>
+					<button
+						onClick={() => setOffset(12)}
+						className={offset === 12 ? styles.active : ''}>
+						2
+					</button>
 				</div>
 			</Wrapper>
 		</section>
